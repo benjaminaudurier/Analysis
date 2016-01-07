f5d902f2c84560ff64efc62e9ffe91c5a1e23ec9
baudurie <benjamin.audurier@subatech.in2p3.fr>
2016-01-07 16:58:12 +0100

    Cleaning commit : removing temporary compilation files

--- LHC_15n_pp/mergeQA/copyFiles_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./copyFiles_C.so: /Users/audurier/alicesw/root/v5-34-30/inst/include/include/cintdictversion.h /Users/audurier/alicesw/root/v5-34-30/inst/include/include/RVersion.h
-copyFiles_C__ROOTBUILDVERSION= 5.34/30
--- Test1_PbPb2011/MuMuPart/Macro/FNormMacro_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./FNormMacro_C.so: /Users/audurier/alicesw/root/v5-34-30/inst/include/include/cintdictversion.h /Users/audurier/alicesw/root/v5-34-30/inst/include/include/RVersion.h
-FNormMacro_C__ROOTBUILDVERSION= 5.34/30
