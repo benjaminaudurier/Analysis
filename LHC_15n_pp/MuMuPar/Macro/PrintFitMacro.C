068a02c2fd606c11a0e01a523f822c247c688d4d
baudurie <benjamin.audurier@subatech.in2p3.fr>
2016-03-10 09:49:24 +0100

    Cleaning the Macro

--- LHC_15n_pp/MuMuPar/Macro/PrintFitMacro.C
+++ LHC_15n_pp/MuMuPar/Macro/PrintFitMacro.C
@@ -135,18 +135,10 @@ void PrintDist(TObjString* swhat,Bool_t yield,AliAnalysisMuMu &ana)
            Double_t jpsi = spectra->GetResultForBin(swhat->String().Data())->GetValue("NofJPsi");
            // Double_t AccEff = spectra->GetResultForBin(swhat->String().Data())->GetValue("AccEffJPsi");
            Double_t corrjpsi = spectra->GetResultForBin(swhat->String().Data())->GetValue("CorrNofJPsi");
-           Double_t corrjpsistat = spectra->GetResultForBin(swhat->String().Data())->GetErrorStat("CorrNofJPsi");
            Double_t jpsisys = spectra->GetResultForBin(swhat->String().Data())->GetRMS("NofJPsi");
            Double_t jpsistat = spectra->GetResultForBin(swhat->String().Data())->GetErrorStat("NofJPsi");
 
-           // printf("jpsi         =%f\n",jpsi );
-           // // printf("AccEff       =%f\n",AccEff );
-           // printf("corrjpsi     =%f\n",corrjpsi );
-           // printf("corrjpsistat =%f\n",corrjpsistat );
-           // printf("jpsisys      =%f\n",jpsisys );
-           // printf("jpsistat     =%f\n",jpsistat );
-
-           Double_t sigma = jpsi/(lumi*BR*1000.*0.240080);
+           Double_t sigma = corrjpsi/(lumi*BR*1000.);
            Double_t sigmastat = sigma*jpsistat/jpsi;
            Double_t sigmasys2 = jpsisys/jpsi*jpsisys/jpsi  +0.01*0.01 +    0.01*0.01 +    0.02*0.02 +    0.02*0.02;
            //                                                   Tracking    Matching       Trigger        tails
@@ -156,33 +148,15 @@ void PrintDist(TObjString* swhat,Bool_t yield,AliAnalysisMuMu &ana)
             printf("integrated cross section = %f +/- %f +/- %f #mubarn\n",sigma,sigmastat,sigma*TMath::Sqrt(sigmasys2) );
             return;
 
-        }   
-        
-
-           
+        }     
     }
 }
 
 //___________________________________________
 void PrintCrossSection(AliAnalysisMuMuSpectra *spec,AliAnalysisMuMu &an,char* what)
-{
+{   
         // Double_t AccEff[7] =  {0.236,0.228,0.229,0.245,0.284,0.329,0.388134};
-        Double_t AccEff[6] ={0.100832,0.274249,0.365494,0.368718,0.270976,0.080066};
-
-        // TH1* hCrossSection = static_cast<TH1*>(spec->Plot("NofJPsi","",kTRUE)->Clone());
-        // new TCanvas;
-        // hCrossSection->DrawCopy();
-        // return;
-
-        // TH1* hCrossSection = static_cast<TH1*>(spec->Plot("AccEffJPsi","",kTRUE)->Clone());
-        // return;
-    
-        // hCrossSection->Scale(1./(lumi*BR*1000.*lumiTriggerCrossSection));
-        // hCrossSection->SetTitle(Form("J/psi cross section with Lumi. = %f ",lumi));
-        // hCrossSection->GetYaxis()->SetTitle(Form("d^{2}#sigma/dp_{T}dy(#muBxGeV^{-1}xc)"));
-        // hCrossSection->DrawCopy("");
-        // Double_t mul = an.CC()->GetSum(Form("trigger:CMUL7-B-NOPF-MUON/event:PSALL"));
-        // Double_t mb = an.CC()->GetSum(Form("trigger:CINT7/event:PSALL/centrality:V0A"));
+        // Double_t AccEff[6] ={0.100832,0.274249,0.365494,0.368718,0.270976,0.080066};
 
         AliAnalysisMuMuBinning* binning = spec->Binning();
         TObjArray* bins = binning->CreateBinObjArray();
@@ -194,27 +168,20 @@ void PrintCrossSection(AliAnalysisMuMuSpectra *spec,AliAnalysisMuMu &an,char* wh
         const Double_t * binArrayX = binning->CreateBinArrayX();
         Int_t nBinX = binning->GetNBinsX();
 
-        TGraphErrors * hCrossSection = new TGraphErrors(nBinX);
-        TGraphErrors * hRMS = new TGraphErrors(nBinX);
+        TGraphErrors * gCrossSection = new TGraphErrors(nBinX);
+        TGraphErrors * gSys = new TGraphErrors(nBinX);
+        TString sbin;
 
         int j= 1;
         while ( ( bin = static_cast<AliAnalysisMuMuBinning::Range*>(nextBin()) ) )
         {
 
             r = static_cast<AliAnalysisMuMuResult*>(spec->GetResultForBin(bin->AsString()));
+            if(j==1 && bin->AsString().Contains("PT"))sbin ="PT";
+            else if(j==1 && bin->AsString().Contains("Y"))sbin ="Y";
+            printf("sbin = %s\n",sbin.Data() );
 
-            // printf("AccEffJPsi             =%f\n",AccEff[j-1]);
-            // printf("AccEffJPsi framework   =%f\n",r->GetValue("NofJPsi")/r->GetValue("CorrNofJPsi"));
-            // printf("\n");
-            // printf("NofJPsi                = %f\n", r->GetValue("NofJPsi"));
-            // printf("\n");
-            // printf("CorrNofJPsi            = %f\n", r->GetValue("NofJPsi")/(AccEff[j-1]));
-            // printf("CorrNofJPsi  framework = %f\n", r->GetValue("CorrNofJPsi"));
-            // printf("\n");
-            // printf("WidthX                 = %f\n", bin->WidthX());
-
-
-            Double_t sigma =r->GetValue("NofJPsi")/(AccEff[j-1])/(lumi*BR*1000.*bin->WidthX());
+            Double_t sigma =r->GetValue("CorrNofJPsi")/(lumi*BR*1000.*bin->WidthX());
             Double_t sigmaerror = sigma*(r->GetErrorStat("NofJPsi")/r->GetValue("NofJPsi"));
 
              // Get X
@@ -225,58 +192,52 @@ void PrintCrossSection(AliAnalysisMuMuSpectra *spec,AliAnalysisMuMu &an,char* wh
             Double_t error2 =(r->GetRMS("NofJPsi")/r->GetValue("NofJPsi"))*(r->GetRMS("NofJPsi")/r->GetValue("NofJPsi")) +0.01*0.01 + 0.01*0.01 + 0.02*0.02;
             printf(" cross section for bin %s = %f +/- %f +/- %f \n",bin->AsString().Data(), sigma, sigmaerror , sigma*TMath::Sqrt(error2));
 
-            hCrossSection->SetPoint(j,x,sigma);
-            hCrossSection->SetPointError(j,bin->WidthX()/2,sigmaerror); 
+            gCrossSection->SetPoint(j,x,sigma);
+            gCrossSection->SetPointError(j,bin->WidthX()/2,sigmaerror); 
 
-            hRMS->SetPoint(j,x,sigma);
-            hRMS->SetPointError(j,bin->WidthX()/2,sigma*TMath::Sqrt(error2));
+            gSys->SetPoint(j,x,sigma);
+            gSys->SetPointError(j,bin->WidthX()/2,sigma*TMath::Sqrt(error2));
 
             j++;
         }
+        TF1* fit=0x0;
+        TFitResultPtr Fitpoint=0x0;
 
-        // TF1* fitpt1 = new TF1("fitpt1","[0]*x*(1/pow((1+pow(x/[1], [2])) ,[3]))",0,12);
-        // fitpt1->SetParameters(2424.41, 4.74409,1.70488, 4.42257);
+        if(sbin.Contains("PT")){
+            fit = new TF1("fit","[0]*x*(1/pow((1+pow(x/[1], [2])) ,[3]))",0,12);
+            fit->SetParameters(2424.41, 4.74409,1.70488, 4.42257);
+        }
 
-        // TF1* fity1= new TF1("fity1","[0] * TMath::Exp(-0.5*TMath::Power((x-[1])/[2],2.))", -10, 2.5);
-        // fity1->SetParameters(2.8,0.00,2.9271); 
-        // fity1->FixParameter(1,0.00); 
-
+        if(sbin.Contains("Y")){
+            fit= new TF1("fit","[0] * TMath::Exp(-0.5*TMath::Power((x-[1])/[2],2.))", -10, 2.5);
+            fit->SetParameters(2.8,0.00,2.9271); 
+            fit->FixParameter(1,0.00);
+        }
         new TCanvas;
 
-        gPad->SetLogy();
-        hRMS->SetFillColor(2);
-        hRMS->SetFillStyle(3001);
-        hRMS->SetTitle(Form("J/psi cross section"));
-        hRMS->GetYaxis()->SetTitle(Form("d^{2}#sigma/dp_{T}(#muBxGeV^{-1}xc)"));
-        hRMS->GetXaxis()->SetTitle(Form("%s",what));
-        hRMS->SetMarkerSize(1.7);
+        if(sbin.Contains("PT")) gPad->SetLogy();
+        gSys->SetFillColor(2);
+        gSys->SetFillStyle(3001);
+        gSys->SetTitle(Form("J/psi cross section"));
+        if(sbin.Contains("PT")) gSys->GetYaxis()->SetTitle(Form("d^{2}#sigma/dp_{T}(#muBxGeV^{-1}xc)"));
+        if(sbin.Contains("Y")) gSys->GetYaxis()->SetTitle(Form("d^{2}#sigma/dY(#muB)"));
+        gSys->GetXaxis()->SetTitle(Form("%s",what));
+        gSys->SetMarkerSize(1.7);
         
         TLegend * leg = new TLegend(0.4,0.7,0.70,0.9);
         leg->SetHeader(Form("ALICE, p-p #sqrt{s_{NN}}=5.02 TeV, L_{int}=111.31 #mub^{-1})",what));
-        leg->AddEntry(hCrossSection,"Inclusive J/#psi cross-section 2.5 < y < 4","pe"); 
-        // leg->AddEntry(hCrossSection,"Inclusive J/#psi cross-section 0 < pT < 8","pe");    
-           
-        leg->AddEntry(hRMS,"systematic uncertainty ","f");
-        // leg->AddEntry((TObject*)0,"global sys.= 2.23 % (Tracking + Trigger)","");
+        if(sbin.Contains("PT")) leg->AddEntry(gCrossSection,"Inclusive J/#psi cross-section 2.5 < y < 4","pe"); 
+        if(sbin.Contains("Y")) leg->AddEntry(gCrossSection,"Inclusive J/#psi cross-section 0 < pT < 8","pe");               
+        leg->AddEntry(gSys,"systematic uncertainty ","f");
         leg->SetTextSize(0.03);
-        hRMS->DrawClone("A2P[]");
 
-        // TFitResultPtr Fitpoint = hCrossSection->Fit("fitpt1","SR");
-        // printf("cross section = %f +/- %f \n",fitpt1->Integral(0.,8.),fitpt1->IntegralError(0.,8.));
+        gSys->DrawClone("A2P[]");
 
-        // TFitResultPtr Fitpoint = hCrossSection->Fit("fity1","SR");
-        // printf("cross section = %f +/- %f \n",fity1->Integral(-4.,-2.5),fity1->IntegralError(-4.,-2.5));
-
-        // TFitResultPtr Fitpoint = hCrossSection->Fit(fity1,"SR");
-        // printf("cross section = %f +/- %s \n",fity1->Integral(-4, -2.5),fity1->IntegralError(-4, -2.5));
+        if(fit) Fitpoint = gCrossSection->Fit("fit","SR");
+        if(sbin.Contains("PT") && static_cast<int>(Fitpoint)==0) printf("cross section = %f +/- %f \n",fit->Integral(0.,8.),fit->IntegralError(0.,8.));
+        if(sbin.Contains("Y")  && static_cast<int>(Fitpoint)==0)  printf("cross section = %f +/- %f \n",fit->Integral(-4.,-2.5),fit->IntegralError(-4.,-2.5));
         
-
-        hCrossSection->DrawClone("Psame");
+        gCrossSection->DrawClone("Psame");
         leg->DrawClone("same");
-    
-
-    
-    
-    
 }
 
