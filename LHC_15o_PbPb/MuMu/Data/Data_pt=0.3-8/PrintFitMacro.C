90a4351c4c0775a7a58587fedcbbac014301830e
baudurie <benjamin.audurier@subatech.in2p3.fr>
2016-05-20 10:14:53 +0200

    Cleaning LHC15o directory due to new workflow

---
+++ LHC_15o_PbPb/MuMu/Data/Data_pt=0.3-8/PrintFitMacro.C
@@ -0,0 +1,119 @@
+//
+//  PrintFitMacro.c
+//  
+//
+//  Created by Benjamin Audurier on 09/06/15.
+//
+//
+
+// Macro who prints fit results
+
+#include <AliLog.h>
+#include <TObjArray.h>
+#include <TObjString.h>
+#include <AliAnalysisMuMu.h>
+#include <AliAnalysisMuMuSpectra.h>
+#include <AliMergeableCollection.h>
+#include <AliCounterCollection.h>
+#include <TCanvas.h>
+#include <TH1.h>
+#include <TROOT.h>
+#include <iostream> 
+
+//Some strings and constants
+char           * sfile="AnalysisResults.root";
+// char                   * sfile="../Simulation/AnalysisResults.JPSI.root";
+char                   * sasso="../Simulation/AnalysisResults.JPSI.root";
+char                   * sasso2="";
+char                   * beamYear="mumu.PbPb2015.0.3-8.config";
+// char                   * beamYear="mumu.PbPb2015.simu.config";
+
+TString striggerDimuon ="CMUL7-B-NOPF-MUFAST";
+TString striggerMB     ="CINT7-B-NOPF-MUFAST";
+TString seventType     ="PSALL";
+TString spairCut       ="pALLPAIRYPAIRPTIN0.0-12.0RABSMATCHLOWETAPDCA";
+TString scentrality    ="V0M_00.00_90.00";
+
+TString param          =  "sJPsi,mJPsi,NofJPsi,SignalOverBkg3s,FitChi2PerNDF,Significance3s";
+
+void PrintDist(TObjString* swhat,AliAnalysisMuMu &ana);
+
+//_____________________________________________________________________________
+void PrintFitMacro(char         * what ="PT",const char * printWhat = "distribution",int debug =0)
+{    
+
+    TString subresults = "PSINA60NEW,PSICB2";
+
+    AliLog::SetGlobalDebugLevel(debug);
+    
+    Bool_t PrintDistribution = kFALSE;
+    Bool_t Raa               = kFALSE; 
+    Bool_t print             = kFALSE;
+
+    TObjArray* sprint = TString(printWhat).Tokenize(",");
+    TObjArray* sparam = TString(param).Tokenize(",");
+    
+    //Set bool
+    if(sprint->FindObject("raa")) Raa                        =kTRUE;
+    if(sprint->FindObject("distribution")) PrintDistribution =kTRUE;
+    if(sprint->FindObject("save")) print                     =kTRUE;
+
+
+    //General conf.
+    TObjArray* whatArray= TString(what).Tokenize(",");
+    TIter nextWhat(whatArray);
+    TObjString* swhat;
+
+
+    
+    // main object
+    AliAnalysisMuMu analysis(sfile,sasso,sasso2,beamYear);
+
+    //_____ Draw 
+    while ( ( swhat = static_cast<TObjString*>(nextWhat()) ) )
+    {
+        // analysis.DrawFitResults("PSI",swhat->String().Data(),"");
+        // analysis.PrintNofParticle("PSI","NofJPsi",swhat->String(),kFALSE);
+        TIter nextParam(sparam);
+        TObjString* sParam;
+        // /*if(swhat->String().Contains("PT") || swhat->String().Contains("Y")) */while ((sParam=static_cast<TObjString*>(nextParam()))) analysis.PrintFitParam("PSI",sParam->String().Data(),swhat->String().Data(),subresults.Data(),"",kFALSE);
+
+        if (Raa) {
+            if(swhat->String().Contains("INTEGRATED")) analysis.RAAasGraphic("PSI",swhat->String().Data(),"externFile_PT.txt","externFile_CENT.txt",scentrality.Data(),kFALSE);
+            else if(swhat->String().Contains("Y")) analysis.RAAasGraphic("PSI","Y","externFile_Y.txt","externFile_CENT.txt",scentrality.Data(),kFALSE);
+            else if(swhat->String().Contains("PT")) analysis.RAAasGraphic("PSI","PT","externFile_PT.txt","externFile_CENT.txt",scentrality.Data(),kFALSE);
+            else continue;
+        }
+
+        if (PrintDistribution) PrintDist(swhat,analysis);
+    }
+    return ;    
+} 
+
+//___________________________________________
+void PrintDist(TObjString* swhat,AliAnalysisMuMu &ana)
+{
+    
+
+    TObjArray* whatCent= TString(scentrality.Data()).Tokenize(",");
+    TIter nextCent(whatCent);
+    TObjString* scent;
+
+    while ( ( scent = static_cast<TObjString*>(nextCent()) ) ) 
+    {
+        //________Get spectra
+        TString spectraPath= Form("/%s/%s/%s/%s/%s-%s",seventType.Data(),striggerDimuon.Data(),scent->String().Data(),spairCut.Data(),"PSI",swhat->String().Data());
+        printf("--- Centrality = %s\n", scent->String().Data());
+
+        AliAnalysisMuMuSpectra * spectra = static_cast<AliAnalysisMuMuSpectra*>(ana.OC()->GetObject(spectraPath.Data()));
+        if(!spectra)
+        {
+            cout << Form("Cannot find spectra with name %s",spectraPath.Data()) <<endl;
+            return;
+        }
+        new TCanvas;
+        spectra->Plot("NofJPsi","",kTRUE)->DrawCopy("");
+    }
+}
+
+
---
+++ LHC_15o_PbPb/MuMu/Macro/FNormMacro.C
@@ -0,0 +1,76 @@
+/
+//  FnormMacro.c
+//  
+//
+//  Created by Benjamin Audurier on 03/12/14.
+//
+//
+
+// Macro with Fnorm related procedure
+
+
+
+//_____________________________________________________________________________
+void FNormMacro(
+const char* filename="AnalysisResultsNewESDs.root",
+const char* associatedSimFileName="",
+const char* associatedSimFileName2="",
+const char* beamYear="mumu.PbPb2015.config",const int DebugLevel =0)
+{
+
+
+  AliAnalysisMuMu analysis(filename,associatedSimFileName,associatedSimFileName2,beamYear);
+  AliLog::SetGlobalDebugLevel(DebugLevel);
+  // analysis.CleanFNorm();
+  
+  //_____ EquivalentNUmber
+  // analysis.ComputeNumberOfEvent();
+
+  //_____ FNorm
+  
+  // analysis.ComputeNumberOfEvent(); // kTRUE = PileUpCOrrected
+  // analysis.ComputeIntFnormFromCounters(AliAnalysisMuMuFnorm::kMUL,kTRUE); // kTRUE = PileUpCOrrected
+  // analysis.ComputeFnormScalers(AliAnalysisMuMuFnorm::kMUL,kTRUE);// kTRUE = PileUpCOrrected
+
+  //_____ Mean
+  // const char* patternOrList= "FnormOffline2PUPS,FnormOffline1PUPS,FnormScalers1PUPS";
+  // analysis.ComputeFnormWeightedMeanGraphs(AliAnalysisMuMuFnorm::kMUL,patternOrList);
+  //_____ 
+    
+  // analysis.ComputeJpsiYield("INTEGRATED");
+  
+
+  //_____ Plot all Fnorm
+  // TH1* h = analysis.OC()->Histo("/FNORM-CPBI1MUL-B-NOPF-MUON/PSALL/V0M_00.00_10.00/hFNormIntVSrun_CCENT_R2-B-NOPF-ALLNOTRD");
+  // if (!h) return;
+  // h->Scale()
+  // h->Draw("Clone"); 
+  
+  PrintCounters(analysis);
+
+  // Compute Mean Fnorm From all Fnorm result   
+  // MeanFnorm(analysis);
+  
+}
+
+//_____________________________________________________________________________
+void PrintCounters(AliAnalysisMuMu &ana)
+{
+    // ana.CC()->Print("trigger/centrality/event","trigger:CVHN-B-NOPF-ALLNOTRD|CVHN_R2-B-NOPF-ALLNOTRD|CCENT-B-NOPF-ALLNOTRD|CCENT_R2-B-NOPF-ALLNOTRD/centrality:V0M_00.00_07.50/event:ALL/run:169557");
+    // ana.CC()->Print("trigger/centrality/event","trigger:CVHN-B-NOPF-ALLNOTRD|CVHN_R2-B-NOPF-ALLNOTRD|CCENT-B-NOPF-ALLNOTRD|CCENT_R2-B-NOPF-ALLNOTRD&0MUL/centrality:V0M_00.00_07.50/event:ALL/run:169557");
+    // ana.CC()->Print("run/trigger/centrality/event","trigger:CVLN_B2-B-NOPF-ALLNOTRD|CVLN-B-NOPF-ALLNOTRD|CVLN_R1-B-NOPF-ALLNOTRD|CSEMI-B-NOPF-ALLNOTRD|CSEMI_R1-B-NOPF-ALLNOTRD&0MUL/centrality:V0M_10.00_50.00/event:ALL");
+    // ana.CC()->Print("run/trigger/centrality/event","trigger:CPBI2_B1-B-NOPF-ALLNOTRD&0MUL/centrality:V0M_00.00_90.00/event:ALL");
+    // ana.CC()->Print("run/trigger","trigger:CINT7-B-NOPF-MUFAST&0MUL/centrality:V0M_00.00_100.00/event:PSALL");
+    // ana.CC()->Print("run/trigger","trigger:CINT7-B-NOPF-MUFAST/centrality:V0M_00.00_100.00/event:PSALL"); 
+    // ana.CC()->Print("run/trigger","trigger:CINT7-B-NOPF-MUFAST&0MUL/centrality:V0M_00.00_90.00/event:ALL");
+    // ana.CC()->Print("run/event","trigger:CINT7-B-NOPF-MUFAST/centrality:V0M_00.00_90.00");
+    // ana.CC()->Print("run/event","trigger:CINT7-B-NOPF-MUFAST&0MUL/centrality:V0M_00.00_90.00");   
+    // ana.CC()->Print("run/event","trigger:CMUL7-B-NOPF-MUFAST/centrality:V0M_00.00_90.00");
+    ana.CC()->Print("centrality/event/trigger");
+    // ana.CC()->Print("centrality/event","trigger:C0V0M-B-NOPF-CENTNOTRD&0MUL");
+    // Int_t NofMUL = TMath::Nint(ana.CC()->GetSum(Form("trigger:CMUL7-B-NOPF-MUFAST/event:PSALL/centrality:V0M_00.00_90.00")));
+    // cout <<"Nofmul = " << NofMUL << endl;   
+    // ana.CC()->Print("");
+    // ana.CC()->PrintKeyWords();
+}
+
